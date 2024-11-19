20d0c8bcf6033dcf8806c3ff949ca8bcff6264f8
ChristopherLMiller <chris@christopherleemiller.me>
2024-11-19 10:23:31 -0500

    fix: :bug: Menu order field non-unique; graphQL introspection

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,6 +6,19 @@ export default ({ env }) => ({
       sendMetadata: true,
     },
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
   upload: {
     config: {
       provider: 'strapi-provider-cloudflare-r2',
