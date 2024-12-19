e45bfeaf0cbbccaa7be9ed6217058fb73fc01547
ChristopherLMiller <chris@christopherleemiller.me>
2024-12-19 01:09:07 -0500

    build: :arrow_up: Update deps

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -552,10 +552,11 @@ export interface ApiPostPost extends Struct.CollectionTypeSchema {
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::post.post'> &
       Schema.Attribute.Private;
-    post_category: Schema.Attribute.Relation<
+    postCategory: Schema.Attribute.Relation<
       'oneToOne',
       'api::post-category.post-category'
     >;
+    publicationDate: Schema.Attribute.DateTime;
     publishedAt: Schema.Attribute.DateTime;
     seo: Schema.Attribute.Component<'shared.seo', false>;
     slug: Schema.Attribute.UID<'title'> & Schema.Attribute.Required;
